141fd65e4ee2af7e75051630b7e6ae972c031341
sam w <sam@foofrancine.com>
2015-07-25 17:33:05 -0500

    initial commit

---
+++ app.rb
@@ -0,0 +1,40 @@
+require 'json'
+require "sinatra"
+require "data_mapper"
+require 'dm-migrations'
+require 'sinatra/flash'
+
+enable :sessions
+
+configure :development do
+  DataMapper.setup(
+    :default, 'postgres://user:pi@localhost/test'
+  )
+end
+
+configure :production do
+  DataMapper.setup (
+    :default, 'postgres://postgres:123@localhost/sinatra_service'
+  )
+end
+
+
+class Weather
+  include DataMapper::Resource
+  property :id,  Serial
+  property :content, String
+  property :temperature, String
+  property :humidity, String
+  property :completed_at, DateTime
+  property :created_at, DateTime
+end
+
+DataMapper.finalize
+Weather.auto_upgrade!
+
+get "/records/?" do
+  $records = Weather.all(:order => :created_at.desc)
+  erb :"weather/index"
+end
+
+
